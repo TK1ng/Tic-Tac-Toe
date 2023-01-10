eade16494a8476c9f8436879c0999054822862b3
Taryn King <marshaeking@gmail.com>
2023-01-10 15:51:28 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Square from './components/Square';
 
 function App() {
+  const user = 'King';
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Let's play Tic-Tac-Toe!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Square propVar={user} />
     </div>
   );
 }
---
+++ src/components/Square.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Square = ({propVar}) => {
+    return ( 
+        <div className="square">
+            Hi, {propVar}!
+        </div>
+     );
+}
+ 
+export default Square;
