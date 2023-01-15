94be0e8dc5011103b06e2d3dab413f7e1e5cc213
Taryn King <marshaeking@gmail.com>
2023-01-14 19:16:49 -0600

    style:add css modules in square component

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,7 @@
-import './Square.css';
+import styles from './Square.module.css';
 
 
 const Square = (props) => {
-
-    // const game = useContext(GameContext);
 
     const handleClick = () => {
         if (!props.squareValue) {
@@ -20,7 +18,7 @@ const Square = (props) => {
     }
 
     return ( 
-        <button className="square" onClick={handleClick} disabled={props.gameStatus}>
+        <button className={styles.square} onClick={handleClick} disabled={props.gameStatus}>
           {props.squareValue === 'O' ? <img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" alt="DevMountain logo"/> : props.squareValue}
         </button>
      );
